9db5eb961ae9dc11062f499825c3aebce2ce730a
Matheus Oliveira <matheusluis.dev@gmail.com>
2023-05-16 14:28:33 -0300

    Liskov Substitution Principle

--- StudyingDesignPatternsDotNet/Program.cs
+++ StudyingDesignPatternsDotNet/Program.cs
@@ -1,6 +1,6 @@
 ﻿using StudyingDesignPatternsDotNet;
 
-string[] files = { "01_SingleResponbilityPrinciple.cs", "02_OpenClosedPrinciple.cs" };
+string[] files = { "01_SingleResponbilityPrinciple.cs", "02_OpenClosedPrinciple.cs", "03_LiskovSubstitutionPrinciple.cs" };
 
 Console.WriteLine("Choose a file:\n");
 foreach (var file in files)
@@ -19,5 +19,8 @@ switch (choosed)
         break;
     case 2:
         new Demo02().Main(default);
+        break;
+    case 3:
+        new Demo03().Main(default);
         break;
 }
